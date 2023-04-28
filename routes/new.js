e667ecdaebe369bdc0b6639bfc4e56e963ef0180
demiladeore <oluwafemidemi47@gmail.com>
2023-04-29 00:07:44 +0100

    add  new html file, change file directory in new.js route

--- routes/new.js
+++ routes/new.js
@@ -1,9 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var path = require("path");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "../views/news.html"));
 });
 
 module.exports = router;
