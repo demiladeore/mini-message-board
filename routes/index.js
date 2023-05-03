707d5b43b8d10d131250d54124b79a9e5a10646d
demiladeore <oluwafemidemi47@gmail.com>
2023-05-03 13:12:43 +0100

    add new message form and styles

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,35 @@
 var express = require("express");
 var router = express.Router();
+const path = require("path");
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date().toLocaleTimeString(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date().toLocaleTimeString(),
+  },
+];
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  const messages = [
-    {
-      text: "Hi there!",
-      user: "Amando",
-      added: new Date(),
-    },
-    {
-      text: "Hello World!",
-      user: "Charles",
-      added: new Date(),
-    },
-  ];
   res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
+// GET add new message page
+
+router.get("/new", function (req, res) {
+  // res.sendFile(path.join(__dirname, "../views/new.html"));
+  res.render("new", { title: "New Messageboard" });
+});
+router.post("/new", function (req, res) {
+  console.log(req.body);
+  req.body["added"] = new Date().toLocaleTimeString();
+  messages.push(req.body);
+  res.redirect("/");
+});
 module.exports = router;
